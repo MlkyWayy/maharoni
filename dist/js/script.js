5e9b909f90ad7c65eb94da53ce6c1d5bed4d812c
Fajar M <habibimochfajar.ah@gmail.com>
2023-10-19 02:16:18 +0700

    tambah halaman login

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,21 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+//Login
+const signin = document.querySelector(".login");
+const signup = document.querySelector(".signup");
+const btnSignin = document.querySelector("#signin");
+const btnSignup = document.querySelector("#signup");
+const img = document.querySelector(".loginimg");
+
+btnSignup.onclick = () => {
+  signin.classList.add("skanan", "hidden");
+  img.classList.add("pindah");
+  signup.classList.remove("skiri", "hidden");
+};
+btnSignin.onclick = () => {
+  signin.classList.remove("skanan", "hidden");
+  signup.classList.add("skiri", "hidden");
+  img.classList.remove("pindah");
+};
